8064decadebdbc6d0880f695d62edf0d9dbedb90
nik-55 <nik.xyz.in@gmail.com>
2024-07-15 04:20:11 +0530

    chore: conditional node using scopes

--- v2/executors/conditional_node.py
+++ v2/executors/conditional_node.py
@@ -1,27 +1,45 @@
+import requests
+
 from v2.executors.base import Base
 from v2.executors.utils import get_data
+
+from v2.flow_manager import FlowManager
 
 
 class ConditionalNode(Base):
     def execute(self) -> dict:
         inputs = self.inputs
-        input_slot = self.node.input_slots[0]
-        value_type = input_slot.get("value_type")
-        value = inputs.get(input_slot.get("name"))
-        typecast_value = get_data(value_type, value)
+        value_type = self.node.dict.get("value_type")
 
-        output_slots = self.node.output_slots
-        output_values = self.node.dict.get("values")
+        condition = inputs.pop("_condition")
+        condition_typecast_value = get_data(value_type, condition)
 
-        outputs = {}
+        cases = self.node.dict.get("cases")
 
-        for output_slot in output_slots:
-            output_value = output_values.get(output_slot.get("name"))
-            value_type = output_slot.get("value_type")
-            typecast_output_value = get_data(value_type, output_value)
+        for case in cases:
+            value = case.get("value")
+            block = case.get("block")
 
-            if typecast_value == typecast_output_value:
-                outputs[output_slot.get("name")] = typecast_output_value
+            if block.get("flow").get("name") == "_default":
+                match_case = case
+                continue
+
+            case_typecast_value = get_data(value_type, value)
+
+            if condition_typecast_value == case_typecast_value:
+                match_case = case
                 break
+
+        flow_id = match_case.get("block").get("flow").get("id")
+        res = requests.get(f"http://localhost:8000/v2/flow/{flow_id}/nodes/")
+        data = res.json()
+
+        flow = data.get("flow")
+        nodes = data.get("nodes")
+        inputs["_case"] = condition_typecast_value
+        print(f"Matched case: {flow.get('name')}")
+        flow_manager = FlowManager(flow, nodes, inputs)
+        flow_manager.manage()
+        outputs = flow_manager.outputs
 
         return outputs
